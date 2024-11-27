011f6abbcf985c8ab8d4cb66e5469d6fc64db643
Kéwan B <mail@kewan.fr>
2024-11-27 15:37:50 +0100

    getTimeFileName

---
+++ utils/functions.js
@@ -0,0 +1,13 @@
+export function getTimeFileName() {
+
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+}
