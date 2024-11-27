c0142ff2c14819891dc8e97b6e79fe8463ad6ac3
Kéwan B <mail@kewan.fr>
2024-11-27 10:20:36 +0100

    config

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ export default {
     webhookURL: "",
     DATA_URL: "https://notes.iut-nantes.univ-nantes.fr/services/data.php?q=dataPremièreConnexion",
     LOGIN_PAGE_URL: "https://notes.iut-nantes.univ-nantes.fr/services/doAuth.php?href=https%3A%2F%2Fnotes.iut-nantes.univ-nantes.fr%2F",
-    dataPath: "./data/phpsession.json",
+    PHPSESSION_PAGE: "./data/phpsession.json",
     HEADLESS: false,
     
     login: {
