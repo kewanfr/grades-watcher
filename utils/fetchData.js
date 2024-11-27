587206e798a0ae52b98ddf86581b536e45516395
Kéwan B <mail@kewan.fr>
2024-11-27 10:20:49 +0100

    fetchData from the API after login

---
+++ utils/fetchData.js
@@ -0,0 +1,36 @@
+import config from "../config.js";
+
+import getPHPSession from "./getPHPSession.js";
+const REFRESH_PHPSESSION = true;
+
+export default async function fetchData() {
+
+    let PHPSESSID = await getPHPSession();
+    if (!PHPSESSID) {
+        PHPSESSID = await getPHPSession(REFRESH_PHPSESSION);
+    }
+
+
+    const URL = config.DATA_URL;
+
+    const response = await fetch(URL, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cookie': `PHPSESSID=${PHPSESSID}`
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.redirect) {
+        PHPSESSID = await getPHPSession(REFRESH_PHPSESSION);
+        return await fetchData();   
+    }
+
+    return data;
+}
