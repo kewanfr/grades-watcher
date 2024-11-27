1dc958e5307268aa8fe1d25fb285588c68b47926
Kéwan B <mail@kewan.fr>
2024-11-27 15:53:55 +0100

    saveReleve only save if there are changes

--- utils/saveReleve.js
+++ utils/saveReleve.js
@@ -2,23 +2,27 @@ import fs from 'fs';
 import config from '../config.js';
 import { getTimeFileName } from './functions.js';
 import path from 'path';
+import { compareReleve } from './compareReleve.js';
 
 export async function saveReleve(releve) {
 
     const fileName = getTimeFileName() + ".txt";
     const filePath = path.join(config.OLD_RELEVE_FOLDER, fileName);
 
+    const comparaison = await compareReleve(releve);
+
+    if (comparaison.length == 0) return false
+
     await fs.writeFileSync(filePath, JSON.stringify(releve, null, 2));
 
     if (!fs.existsSync(config.RELEVE_FILE)) {
         return await fs.writeFileSync(config.RELEVE_FILE, JSON.stringify(releve, null, 2));
     }
-    
+
     const oldReleve = await fs.readFileSync(config.RELEVE_FILE, 'utf8');
 
-    await fs.writeFileSync(config.LAST_RELEVE_FILE, oldReleve)
+-   await fs.writeFileSync(config.LAST_RELEVE_FILE, oldReleve)
     return await fs.writeFileSync(config.RELEVE_FILE, JSON.stringify(releve, null, 2));
 
     
-
 }
