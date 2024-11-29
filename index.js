5ff3170e040fc7d86edec54979b7905f52ded054
Kéwan B <mail@kewan.fr>
2024-11-29 07:31:58 +0100

    function watchForNote

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import fs from "fs";
 import { saveReleve } from "./utils/saveReleve.js";
 import sendDiscordMessage from "./discord/sendMessage.js";
 import { Colors, EmbedBuilder } from "discord.js";
+import { getTimeForLog } from "./utils/functions.js";
 
 if (!fs.existsSync("./releves/old")) {
   fs.mkdirSync("./releves/old", { recursive: true });
@@ -16,18 +17,20 @@ if (!fs.existsSync("./data")) {
   fs.mkdirSync("./data");
 }
 
-async function main() {
+async function watchForNote() {
   const data = await fetchData();
 
-//   // await fs.writeFileSync("./data/data.json", JSON.stringify(data))
-
-//   const data = JSON.parse(await fs.readFileSync("./data/data.json"));
+  if (!data) return false
 
 
   const parsed = await parseReleve(data);
   const resultSave = await saveReleve(parsed);
 
-  if (!fs.existsSync(config.LAST_RELEVE_FILE)) return true;
+  if (!fs.existsSync(config.LAST_RELEVE_FILE)) {
+    console.log(`[${getTimeForLog()}] Aucun nouvelle note`);
+    
+    return true;
+  }
 
   const lastReleveData = await fs.readFileSync(
     config.LAST_RELEVE_FILE,
@@ -39,69 +42,50 @@ async function main() {
     JSON.parse(lastReleveData)
   );
 
+  if (comparaison.length == 0 || comparaison == false) {
+    console.log(`[${getTimeForLog()}] Aucun nouvelle note`);
+    return false
+  }
+
   console.log("comparaison", comparaison);
 
   for (let nt of comparaison) {
     if (nt.oldNote.note == "~") {
+      const embed = new EmbedBuilder()
+        .setTitle(`Nouvelle note`)
+        .setDescription(
+          `${nt.ressource} - ${nt.note.description} | **${nt.note.note}**`
+        )
+        .setColor(Colors.Blurple)
+        .addFields(
+          {
+            name: "Note",
+            value: nt.note.note,
+            inline: false,
+          },
+          {
+            name: "Note Minimum",
+            value: nt.note.noteMin,
+            inline: true,
+          },
+          {
+            name: "Note Maximum",
+            value: nt.note.noteMax,
+            inline: true,
+          },
+          {
+            name: "Moyenne",
+            value: nt.note.moy,
+            inline: true,
+          }
+        );
 
-        const embed = new EmbedBuilder()
-            .setTitle(`Nouvelle note`)
-            .setDescription(`${nt.ressource} - ${nt.note.description} | **${nt.note.note}**`)
-            .setColor(Colors.Blurple)
-            .addFields(
-                {
-                    name: "Note",
-                    value: nt.note.note,
-                    inline: false,
-                  },
-                {
-                    name: "Note Minimum",
-                    value: nt.note.noteMin,
-                    inline: true
-                },
-                {
-                    name: "Note Maximum",
-                    value: nt.note.noteMax,
-                    inline: true,
-                  },
-                  {
-                    name: "Moyenne",
-                    value: nt.note.moy,
-                    inline: true,
-                  },
-            );
-        
-        const embedGlobal = new EmbedBuilder()
-            .setTitle(`Nouvelle note`)
-            .setDescription(`${nt.ressource} - ${nt.note.description} | **${nt.note.note}**`)
-            .setColor(Colors.Blurple)
-            .addFields(
-                {
-                    name: "Note",
-                    value: nt.note.note,
-                    inline: false,
-                  },
-                {
-                    name: "Note Minimum",
-                    value: nt.note.noteMin,
-                    inline: true
-                },
-                {
-                    name: "Note Maximum",
-                    value: nt.note.noteMax,
-                    inline: true,
-                  },
-                  {
-                    name: "Moyenne",
-                    value: nt.note.moy,
-                    inline: true,
-                  },
-            );
+      await sendDiscordMessage([embed], null, 1);
 
-      await sendDiscordMessage(null, [embed])
+      console.log("Nouvelle note", nt);
+    } else {
+      console.log(`[${getTimeForLog()}] Aucun nouvelle note`)
     }
   }
-
 }
 
-main();
