894021668a976a03f976ecd678c5470dabff5465
Kéwan B <mail@kewan.fr>
2024-11-29 07:31:45 +0100

    getTimeForLog

--- utils/functions.js
+++ utils/functions.js
@@ -11,3 +11,17 @@ export function getTimeFileName() {
 
     return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
 }
+
+export function getTimeForLog() {
+
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
