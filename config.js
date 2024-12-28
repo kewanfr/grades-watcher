5b1a0fa87463ed88457f30c0d95b666deed08014
Kéwan B <mail@kewan.fr>
2024-12-28 02:44:30 +0100

    fetchNotesList

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ export default {
 
     webhookURL: "",
     DATA_URL: "https://notes.iut-nantes.univ-nantes.fr/services/data.php?q=dataPremièreConnexion",
+    NOTES_LIST_URL: "https://notes.iut-nantes.univ-nantes.fr/services/data.php?q=listeNotes&eval=",
     LOGIN_PAGE_URL: "https://notes.iut-nantes.univ-nantes.fr/services/doAuth.php?href=https%3A%2F%2Fnotes.iut-nantes.univ-nantes.fr%2F",
     URL_SITE: "https://notes.iut-nantes.univ-nantes.fr/",
 
---
+++ utils/fetchNotesList.js
@@ -0,0 +1,41 @@
+import config from "../config.js";
+
+import getPHPSession from "./getPHPSession.js";
+const REFRESH_PHPSESSION = true;
+
+export default async function fetchNotesList(evaluationId) {
+
+    let PHPSESSID = await getPHPSession();
+    if (!PHPSESSID) {
+        PHPSESSID = await getPHPSession(REFRESH_PHPSESSION);
+    }
+
+
+    const URL = config.NOTES_LIST_URL;
+
+    try {
+        const response = await fetch(URL + evaluationId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Cookie': `PHPSESSID=${PHPSESSID}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    
+        const data = await response.json();
+    
+        if (data.redirect) {
+            PHPSESSID = await getPHPSession(REFRESH_PHPSESSION);
+            return await fetchData();   
+        }
+    
+        return data;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
