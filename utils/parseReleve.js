dc4631422c60aeaf6f774fecdf38d60c01db4e08
Kéwan B <mail@kewan.fr>
2025-03-22 15:10:16 +0100

    fix

--- utils/parseReleve.js
+++ utils/parseReleve.js
@@ -2,87 +2,87 @@ import fs from "fs";
 
 
 export async function parseReleve(dataReleve) {
+  // console.log("dataReleve", JSON.stringify(dataReleve, null, 2));
+  await fs.writeFileSync(
+    "./dataAll.json",
+    JSON.stringify(dataReleve, null, 2)
+  );
+  if (dataReleve["relevé"]) {
+    dataReleve = dataReleve["relevé"];
+  }
 
+  var res = {};
 
-    // console.log("dataReleve", JSON.stringify(dataReleve, null, 2));
-    await fs.writeFileSync("./dataAll.json", JSON.stringify(dataReleve, null, 2));
-    if (dataReleve["relevé"]){
-        dataReleve = dataReleve["relevé"]
-    }
+  res.summary = {
+    moyenne: dataReleve.semestre?.notes?.value,
+    min: dataReleve.semestre?.notes?.min,
+    moy_promo: dataReleve.semestre?.notes?.moy,
+    max_promo: dataReleve.semestre?.notes?.max,
+    rang: dataReleve.semestre?.rang?.value,
+  };
 
-    var res = {};
+  res.ressources = {};
+  res.saes = {};
+  // console.log("dataReleve", dataReleve);
+  // console.log("ressources", dataReleve.ressources);
+  // console.log("ressources", Object.entries(dataReleve.ressources));
 
-    res.summary = {
-        moyenne: dataReleve.semestre?.notes?.value,
-        min: dataReleve.semestre?.notes?.min,
-        moy_promo: dataReleve.semestre?.notes?.moy,
-        max_promo: dataReleve.semestre?.notes?.max,
-        rang: dataReleve.semestre?.rang?.value,
-    }
-
-    res.ressources = {}
-    res.saes = {}
+  if (!dataReleve.ressources) {
+    console.log("pas de ressources");
     console.log("dataReleve", dataReleve);
-    console.log("ressources", dataReleve.ressources);
-    console.log("ressources", Object.entries(dataReleve.ressources));
-
-    for (const [key, value] of Object.entries(dataReleve.ressources)) {
-      console.log("ressources", key, value);
-
-      const notes = [];
-
-      for (const [ke, val] of Object.entries(value.evaluations)) {
-        notes.push({
-          id: val.id,
-          description: val.description,
-          note: val.note.value,
-          noteMin: val.note.min,
-          noteMax: val.note.max,
-          moy: val.note.moy,
-          // coef: val.note.coef,
-          poids: val.poids,
-        });
-      }
-
-      res.ressources[key] = {
-        key: key,
-        nom: value.titre,
-        notes: notes,
-      };
-    }
-
-
-
-
-    for (const [key, value] of Object.entries(dataReleve.saes)) {
-
-
-        const notes = [];
-
-        for (const [ke, val] of Object.entries(value.evaluations)) {
-
-            notes.push({
-                id: val.id,
-                description: val.description,
-                note: val.note.value,
-                noteMin: val.note.min,
-                noteMax: val.note.max,
-                moy: val.note.moy,
-                // coef: val.note.coef,
-                poids: val.poids,
-            });
-        }
-
-
-        res.saes[key] = {
-            key: key, 
-            nom: value.titre,
-            notes: notes,
-        }
-    }
-
-    // await saveReleve(res);
-
+    res.ressources = {};
     return res;
+  }
 
+  for (const [key, value] of Object.entries(dataReleve.ressources)) {
+    //   console.log("ressources", key, value);
+
+    const notes = [];
+
+    for (const [ke, val] of Object.entries(value.evaluations)) {
+      notes.push({
+        id: val.id,
+        description: val.description,
+        note: val.note.value,
+        noteMin: val.note.min,
+        noteMax: val.note.max,
+        moy: val.note.moy,
+        // coef: val.note.coef,
+        poids: val.poids,
+      });
+    }
+
+    res.ressources[key] = {
+      key: key,
+      nom: value.titre,
+      notes: notes,
+    };
+  }
+
+  for (const [key, value] of Object.entries(dataReleve.saes)) {
+    const notes = [];
+
+    for (const [ke, val] of Object.entries(value.evaluations)) {
+      notes.push({
+        id: val.id,
+        description: val.description,
+        note: val.note.value,
+        noteMin: val.note.min,
+        noteMax: val.note.max,
+        moy: val.note.moy,
+        // coef: val.note.coef,
+        poids: val.poids,
+      });
+    }
+
+    res.saes[key] = {
+      key: key,
+      nom: value.titre,
+      notes: notes,
+    };
+  }
+
+  // await saveReleve(res);
+
+  return res;
 }
